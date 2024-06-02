f6d9dc08e8f88406baf05db971650bc86e286af6
Georgi <gogolino1982@gmail.com>
2024-06-02 21:23:57 +0300

    First test add

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,5 +1,12 @@
 const { test, expect } = require("@playwright/test");
 
 test('Verify "All Books" link is visible', async ({ page }) => {
+    // Open app
     await page.goto('http://localhost:3000');
+    // locate page navbar
+    await page.waitForSelector('.navbar');
+    const allBooksLink = await page.$('a[href="/catalog"]');
+    const isLinkVisible = await allBooksLink.isVisible();
+    expect(isLinkVisible).toBe(true);
+
 });
